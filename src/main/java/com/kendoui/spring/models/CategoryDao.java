0082cea195ed872d45f552bdbc14fa9fa1dfc8c1
levent.yildiz <levent.yildiz@turkkep.com.tr>
2015-02-02 12:15:30 +0200

    initial commit

---
+++ src/main/java/com/kendoui/spring/models/CategoryDao.java
@@ -0,0 +1,9 @@
+package com.kendoui.spring.models;
+
+import java.util.List;
+
+public interface CategoryDao {
+    public DataSourceResult getList(DataSourceRequest request);
+
+    public List<Category> getList();
+}
